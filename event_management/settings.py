7d873507ff3cf3fa154dea60b1d035775e8a81e0
Md-Mansor <mansor.me777@gmail.com>
2025-02-19 17:46:26 +0600

    database setup complete

--- event_management/settings.py
+++ event_management/settings.py
@@ -81,6 +81,16 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'event_management',
+        'USER': 'postgres',
+        'PASSWORD': 'mansor@777',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
