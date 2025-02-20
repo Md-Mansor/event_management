9c67176342fc14d754d941ff42b1a8c224ca7dd5
Md-Mansor <mansor.me777@gmail.com>
2025-02-20 18:36:28 +0600

    Models Created

--- event/models.py
+++ event/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField()
+
+class Event(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField()
+    date=models.DateField()
+    time=models.TimeField()
+    location=models.CharField(max_length=100)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+
+class Participant(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+    event=models.ManyToManyField(Event)
